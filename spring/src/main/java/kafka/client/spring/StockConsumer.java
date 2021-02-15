30eae3cb3e559e876a19a655bd7bae953ae1e83f
Elvis de Freitas <edigitalb@gmail.com>
2021-02-14 22:23:02 -0300

    fixing spring

--- spring/src/main/java/kafka/client/spring/StockConsumer.java
+++ spring/src/main/java/kafka/client/spring/StockConsumer.java
@@ -1,23 +1,18 @@
 package kafka.client.spring;
 
 import com.mageddo.kafka.client.ConsumeCallback;
-import com.mageddo.kafka.client.Consumers;
+import com.mageddo.kafka.client.Consumer;
+import com.mageddo.kafka.client.ConsumerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
 
 @Component
-public class StockConsumer implements InitializingBean {
+public class StockConsumer implements Consumer {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
-  private final Consumers<String, String> consumers;
-
-  public StockConsumer(Consumers<String, String> consumers) {
-    this.consumers = consumers;
-  }
 
   ConsumeCallback<String, String> consume() {
     return (ctx, record) -> {
@@ -26,14 +21,13 @@ public class StockConsumer implements InitializingBean {
   }
 
   @Override
-  public void afterPropertiesSet() {
-    this.consumers
-      .toBuilder()
+  public ConsumerConfig<String, String> config() {
+    return ConsumerConfig
+      .<String, String>builder()
       .consumers(3)
       .prop(GROUP_ID_CONFIG, "spring_stock")
       .topics("stock_changed")
       .callback(this.consume())
-      .build()
-      .consume();
+      .build();
   }
 }
